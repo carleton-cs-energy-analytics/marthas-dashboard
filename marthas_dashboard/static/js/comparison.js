d7089aac7f376f1468ace750f669b63dcb927031
Jack Lightbody <jack.lightbody@gmail.com>
2018-01-17 13:23:35 -0600

    Lots of work to clean up comparisons. Can now close #11

--- marthas_dashboard/static/js/comparison.js
+++ marthas_dashboard/static/js/comparison.js
@@ -1,33 +1,37 @@
-var button = document.getElementsByClassName("add-comparison")[0];
+var add_comparison = document.getElementsByClassName("add-comparison")[0];
+var close_comparison = document.getElementsByClassName("close-comparison")[1];
 var comparison = document.getElementsByClassName("comparison")[0];
 var forms = document.getElementsByClassName("filter_data");
-console.log(forms)
-button.addEventListener('click', function(event){
+add_comparison.addEventListener('click', function(event){
     comparison.style.display = 'block';
+    add_comparison.style.display = 'none';
 });
-for(var form in forms){
-    form.addEventListener("submit", constructComparisonUrl());
+close_comparison.addEventListener('click', function(event){
+    comparison.style.display = 'none';
+    add_comparison.style.display = 'block';
+});
+for(var i = 0; i < forms.length; i++){
+    forms[i].addEventListener("submit", constructComparisonUrl);
 }
 function constructComparisonUrl(event){
     event.preventDefault();
-    var prepend = ''
     var inputs = '?'
+    console.log(forms)
     for (var i = 0; i < forms.length; i++) {
         var form = forms[i];
         var form_inputs = form.getElementsByTagName("input");
-        var form_selects = form.getElementsByTagName("input");
+        var form_selects = form.getElementsByTagName("select");
 
         for(var j = 0; j < form_inputs.length; j++){
-            inputs = inputs + addInputUrl(prepend, form_inputs[j])
+            inputs = inputs + addInputUrl(form_inputs[j])
         };
         for(var j = 0; j < form_selects.length; j++){
-            inputs = inputs + addInputUrl(prepend, form_selects[j])
+            inputs = inputs + addInputUrl(form_selects[j])
         };
         // get all elements
-        prepend = 'c_';
     };
-    window.location.href = window.location.protocol + "//" + window.location.host + "/comparison"+inputs; 
+    window.location.href = window.location.protocol + "//" + window.location.host + "/search"+inputs; 
 }
-function addInputUrl(prepend, el){
-    return prepend+el.name+'='+el.value+'&';
+function addInputUrl(el){
+    return el.name+'='+el.value+'&';
 }
--- marthas_dashboard/views.py
+++ marthas_dashboard/views.py
@@ -9,13 +9,13 @@ from .api import API
 api = API()
 
 
-@app.route('/test3')
+@app.route('/')
 def index():
-    return redirect(url_for('live'))
+    return redirect(url_for('search'))
 
 
-@app.route('/live')
-def live():
+@app.route('/search')
+def search():
     building_names = {'2': 'Libe'}
     pt_names = {
         "511": "LAB HOT H2O TEMP",
@@ -24,85 +24,23 @@ def live():
         "514": "STATIC SET",
     }
 
-    # Get arguments from URL, if any
-    args = request.args
-    building = args.get('building', default='2')
-    point = args.get('point', default='511')
-    start_date = args.get('_from', default='2016-08-18')
-    end_date = args.get('to', default='2017-08-19')
+    searches = create_search_bins(request.args)
 
-    data = api.point_values(point, start_date, end_date)
+    if len(searches) < 1:
+        searches[0] = {}
+        # Just set some defaults if we didn't have any searches
+        searches[0]['building'] = '2'
+        searches[0]['point'] = '511'
+        searches[0]['from'] = '2016-08-18'
+        searches[0]['to'] = '2017-08-19'
 
-    if len(data) <= 1:
-        # Todo: Implement flask "flash" here
-        # See: http://flask.pocoo.org/docs/0.12/patterns/flashing/
-        return "No data for that point"
-
-    fig = generate_figure(data['pointtimestamp'], data['pointvalue'])
-    script, div = components(fig)
-    html = render_template(
-        'chart.html',
-        plot_scripts=[script, script],
-        plot_divs=[div, div],
-        building_names=[building_names[building], building_names[building]],
-        point_names=[pt_names[point],pt_names[point]],
-        start_dates=[start_date, start_date],
-        end_dates=[end_date,end_date],
-        comparison_hidden = True
-    )
-    return encode_utf8(html)
-
-@app.route('/comparison')
-def comparison():
-    building_names = {'2': 'Libe'}
-    pt_names = {
-        "511": "LAB HOT H2O TEMP",
-        "512": "A1 CCL TEMP",
-        "513": "AH2 AIR VOLU",
-        "514": "STATIC SET",
-    }
-
-    # Get arguments from URL, if any
-    args = request.args
-    building = args.get('building', default='2')
-    point = args.get('point', default='511')
-    start_date = args.get('_from', default='2016-08-18')
-    end_date = args.get('to', default='2017-08-19')
-    c_building = args.get('c_building', default='2')
-    c_point = args.get('c_point', default='511')
-    c_start_date = args.get('c_from', default='2016-08-18')
-    c_end_date = args.get('c_to', default='2017-08-19')
-
-    data = api.point_values(point, start_date, end_date)
-    c_data = api.point_values(c_point, c_start_date, c_end_date)
-
-
-    if len(data) <= 1 and len(c_data) <= 1:
-        # Todo: Implement flask "flash" here
-        # See: http://flask.pocoo.org/docs/0.12/patterns/flashing/
-        return "No data for those points"
-
-    fig = generate_figure(data['pointtimestamp'], data['pointvalue'])
-    c_fig = generate_figure(c_data['pointtimestamp'], c_data['pointvalue'])
-
-    script, div = components(fig)
-    c_script, c_div = components(c_fig)
-
-    graph_building_names = [building_names[building], building_names[c_building]]
-    graph_point_names = [pt_names[point], pt_names[c_point]]
-    graph_starts = [start_date, c_start_date]
-    graph_ends = [end_date, c_end_date]
-
+    search_results = do_searches(searches)
+    components = get_components(searches, search_results)
 
     html = render_template(
         'chart.html',
-        plot_scripts=[script, c_script],
-        plot_divs=[div,c_div],
-        building_names=graph_building_names,
-        point_names=graph_point_names,
-        start_dates=graph_starts,
-        end_dates=graph_ends,
-        comparison_hidden=False,
+        components = components,
+        hide_comparison = (len(searches) < 2)
     )
     return encode_utf8(html)
 
@@ -120,3 +58,51 @@ def generate_figure(x, y):
     fig.toolbar.logo = None
     return fig
     # Embed figure in template
+
+
+def create_search_bins(args):
+    search_bins = {}
+    # each arg is formatted as formnum_attribute
+    # where the form number is 0 for original, 1 for first comparison, etc.
+    # and the attribute is something like start_date, end_date
+    # ex 1_start_date is a valid arg
+    # first step is to bin all the attributes for a form together
+    for key, value in args.items(): 
+        search_number = key.split('_')[0]
+        attribute_name = key.replace(search_number+"_", "", 1)
+        search_number = int(search_number)
+        if search_number not in search_bins:
+            search_bins[search_number] = {}
+        search_bins[search_number][attribute_name] = value
+    return search_bins
+
+def do_searches(search_bins):
+    results = []
+    # traverse in order so that the comparison and original don't flip screen position
+    for i in range(len(search_bins)):
+        search = search_bins[i]
+        # in the future we'll want to be able to figure out the type of query that we want to do
+        # based on their restrictions
+        # for now just assume everything is a point values search
+        data = api.point_values(search['point'], search['from'], search['to'])
+        results.append(data)
+    return results
+
+# from all of our data
+# generates plots, scripts and everything that we actually need to inject into the page
+def get_components(searches, search_results):
+    search_results = do_searches(searches)
+    parts = []
+    for i in range(len(search_results)):
+        result = search_results[i]
+        result_components = searches[i]
+        if len(result) <= 1:
+            result_components['plot'] = 'No data for that search'
+            result_components['script'] = ''
+        else:
+            fig = generate_figure(result['pointtimestamp'], result['pointvalue'])
+            result_components['script'], result_components['plot'] = components(fig)
+        parts.append(result_components)
+    return parts
+
+
